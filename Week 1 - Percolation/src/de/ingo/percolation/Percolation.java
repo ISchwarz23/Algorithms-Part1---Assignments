6371e63b9a08e1d7859007c5ea79644dbc824852
Ingo Schwarz <schwarz-ingo@web.de>
2015-09-12 12:29:39 +0200

    Implement corner cases

--- Week 1 - Percolation/src/de/ingo/percolation/Percolation.java
+++ Week 1 - Percolation/src/de/ingo/percolation/Percolation.java
@@ -7,16 +7,19 @@ public class Percolation {
 
     private final boolean[][] field;
 
-    public Percolation(int N) { // create N-by-N grid, with all sites blocked
-        field = new boolean[N][N];
+    public Percolation(int n) { // create N-by-N grid, with all sites blocked
+        if(n <=0) {
+            throw new IllegalArgumentException("N must be at least 1");
+        }
+        field = new boolean[n][n];
     }
 
     public void open(int i, int j) {    // open site (row i, column j) if it is not open already
-        field[i][j] = true;
+        field[i-1][j-1] = true;
     }
 
     public boolean isOpen(int i, int j) {   // is site (row i, column j) open?
-        return field[i][j];
+        return field[i-1][j-1];
     }
 
     public boolean isFull(int i, int j) {   // is site (row i, column j) full?
