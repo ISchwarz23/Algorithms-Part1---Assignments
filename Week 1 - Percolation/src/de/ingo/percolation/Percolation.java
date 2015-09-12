d78b6f71528be9733ee5e43ceee1da8805345f55
Ingo Schwarz <schwarz-ingo@web.de>
2015-09-12 12:50:19 +0200

    Add tests for percolation class

--- Week 1 - Percolation/src/de/ingo/percolation/Percolation.java
+++ Week 1 - Percolation/src/de/ingo/percolation/Percolation.java
@@ -1,29 +1,37 @@
 package de.ingo.percolation;
 
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+
 /**
- * Created by Ingo on 12.09.2015.
+ * The class managing the percolation.
+ * @author ISchwarz
  */
 public class Percolation {
 
-    private final boolean[][] field;
+    private final boolean[][] grid;
+
 
     public Percolation(int n) { // create N-by-N grid, with all sites blocked
         if(n <=0) {
             throw new IllegalArgumentException("N must be at least 1");
         }
-        field = new boolean[n][n];
+        grid = new boolean[n][n];
     }
 
     public void open(int i, int j) {    // open site (row i, column j) if it is not open already
-        field[i-1][j-1] = true;
+        if(!isOpen(i, j)) {
+            // TODO union
+            grid[i - 1][j - 1] = true;
+        }
     }
 
     public boolean isOpen(int i, int j) {   // is site (row i, column j) open?
-        return field[i-1][j-1];
+        return grid[i-1][j-1];
     }
 
-    public boolean isFull(int i, int j) {   // is site (row i, column j) full?
-        return !isOpen(i, j);
+    public boolean isFull(int i, int j) {   // is site (row i, column j) connected to top?
+        return false;
     }
 
     public boolean percolates() {   // does the system percolate?
---
+++ Week 1 - Percolation/test/de/ingo/percolation/PercolationTest.java
@@ -0,0 +1,117 @@
+package de.ingo.percolation;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Tests for the {@link Percolation} class.
+ * @author ISchwarz
+ */
+public class PercolationTest {
+
+    private static final int GRID_SIZE = 5;
+    private Percolation percolation;
+
+    @Before
+    public void setUp() {
+        percolation = new Percolation(GRID_SIZE);
+    }
+
+    @Test
+    public void shouldOpenAGrid() {
+        // given
+        assertFalse(percolation.isOpen(1, 1));
+
+        // when
+        percolation.open(1, 1);
+
+        // then
+        assertTrue(percolation.isOpen(1, 1));
+    }
+
+    @Test
+    public void shouldPercolate() {
+        // when
+        percolation.open(1, 1);
+        percolation.open(1, 2);
+        percolation.open(1, 3);
+        percolation.open(1, 4);
+        percolation.open(1, 5);
+
+        // then
+        assertTrue(percolation.percolates());
+    }
+
+    @Test
+    public void shouldNotPercolate() {
+        // when
+        percolation.open(1, 1);
+        percolation.open(1, 2);
+        percolation.open(1, 4);
+        percolation.open(1, 5);
+
+        // then
+        assertFalse(percolation.percolates());
+    }
+
+    @Test
+    public void shouldBeFull() {
+        // when
+        percolation.open(1, 1);
+        percolation.open(1, 2);
+
+        // then
+        assertTrue(percolation.isFull(1, 2));
+    }
+
+    @Test
+    public void shouldNotBeFull() {
+        // when
+        percolation.open(1, 2);
+
+        // then
+        assertFalse(percolation.isFull(1, 2));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowIllegalArgumentExceptionIfGridSizeIsZero() {
+        new Percolation(0);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowIllegalArgumentExceptionIfGridSizeIsNegative() {
+        new Percolation(-5);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void shouldThrowIndexOutOfBoundsExceptionIfOpenIsCalledWithZero() {
+        percolation.open(0, 0);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void shouldThrowIndexOutOfBoundsExceptionIfOpenIsCalledWithFieldOutsideTheGrid() {
+        percolation.open(GRID_SIZE+1, GRID_SIZE+1);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void shouldThrowIndexOutOfBoundsExceptionIfIsOpenIsCalledWithZero() {
+        percolation.isOpen(0, 0);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void shouldThrowIndexOutOfBoundsExceptionIfIsOpenIsCalledWithFieldOutsideTheGrid() {
+        percolation.isOpen(GRID_SIZE + 1, GRID_SIZE + 1);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void shouldThrowIndexOutOfBoundsExceptionIfIsFullIsCalledWithZero() {
+        percolation.isFull(0, 0);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void shouldThrowIndexOutOfBoundsExceptionIfIsFullIsCalledWithFieldOutsideTheGrid() {
+        percolation.isFull(GRID_SIZE + 1, GRID_SIZE + 1);
+    }
+
+}
